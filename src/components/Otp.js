cc80db23aa73c5d330b90b69704d301b212814af
Anshul Verma <67377491+ThisIsAnshulVerma@users.noreply.github.com>
2022-02-18 13:28:35 +0530

    Added: OTP Validation Page

--- src/components/Otp.js
+++ src/components/Otp.js
@@ -1,29 +1,34 @@
-import { LockClosedIcon } from '@heroicons/react/solid'
+import { LockClosedIcon, BellIcon  } from '@heroicons/react/solid';
 import { render } from '@testing-library/react';
-import React from 'react'
+import React from 'react';
 
-export default function SignIn (props) {
+export default function Otp () {
 
   return (
     <>
-        <p className="mb-10 text-center text-sm font-medium">
-          An OTP will be sent to your email ID for verification
-        </p>
-        <form className="mt-8 space-y-6" onSubmit={props.onClick}>
+        <div className='flex text-center justify-center'>
+            <span>
+                <BellIcon className='h-5 w-5 mx-1 text-red-500 group-hover:text-indigo-400'/>
+            </span>
+            <p className="mb-5 text-sm text-red-700 font-medium">
+                OTP has been sent to your mail account.
+            </p>
+        </div>
+        <form className="space-y-6">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="my-2 rounded shadow-sm -space-y-px">
               <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
+                <label htmlFor="otp" className="sr-only">
+                  OTP
                 </label>
                 <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="otp"
+                  name="otp"
+                  type="text"
+                  autoComplete="otp"
                   required
                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  placeholder="OTP"
                 />
               </div>
               {/* <div>
@@ -46,15 +51,15 @@ export default function SignIn (props) {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                // onClick={props.onClick}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
-                Get OTP
+                
+                Validate
               </button>
             </div>
-          </form>
+        </form>
     </>
   )
 }
